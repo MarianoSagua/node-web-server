11f3734b11603031a5796c0e98b791bdedd8890a
MarianoSagua <marianosagua4343@gmail.com>
2023-07-19 12:27:54 -0300

    Commit 4

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.get("*", (req, res) => {
   res.send("404 | Page not found");
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
